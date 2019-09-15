35e2847de7056bcdffb817a9090672adfccc772f
Toshiaki Maki <tmaki@pivotal.io>
2019-09-16 00:55:11 +0900

    Delete button

--- surveys-frontend/src/App.js
+++ surveys-frontend/src/App.js
@@ -21,24 +21,38 @@ class App extends Component {
         this.loadFromServer();
     }
 
+    deleteSurvey(surveyId) {
+        fetch(`/surveys/${surveyId}`,
+            {
+                method: 'DELETE'
+            })
+            .then(__ => this.loadFromServer());
+    }
+
     render() {
         const surveys = this.state.surveys.map(survey =>
             <tr key={survey.survey_id}>
                 <td>{survey.survey_id}</td>
                 <td>{survey.start_date_time}</td>
                 <td>{survey.start_date_time}</td>
+                <td>
+                    <button onClick={() => this.deleteSurvey(survey.survey_id)}>Delete</button>
+                </td>
             </tr>
         );
         return (
             <div>
                 <p><a href={"/docs/index.html"}>API Document</a></p>
                 <table>
+                    <tbody>
                     <tr>
                         <th>Survey ID</th>
                         <th>Start Date Time</th>
                         <th>End Date Time</th>
+                        <th>Delete</th>
                     </tr>
                     {surveys}
+                    </tbody>
                 </table>
             </div>
         );
