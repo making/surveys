740b689cef4fd63bf56a756c60fa98de71eccbd4
Toshiaki Maki <tmaki@pivotal.io>
2019-09-17 02:43:19 +0900

    [WIP] Add delete button

--- surveys-frontend/src/surveys/Survey.js
+++ surveys-frontend/src/surveys/Survey.js
@@ -1,12 +1,17 @@
 import React, {Component} from 'react';
 import {Panel} from 'pivotal-ui/react/panels';
+import {DangerButton} from 'pivotal-ui/react/buttons';
+import {Redirect} from "react-router-dom";
+import {Icon} from 'pivotal-ui/react/iconography';
 
 class Survey extends Component {
 
     state = {
         survey: {
             survey_questions: []
-        }
+        },
+        disableButton: false,
+        deleted: false
     };
 
     loadFromServer() {
@@ -23,19 +28,56 @@ class Survey extends Component {
         this.loadFromServer();
     }
 
-    deleteSurvey(surveyId) {
-        fetch(`/surveys/${surveyId}`,
+    deleteSurvey() {
+        this.setState({
+            disableButton: true
+        });
+        fetch(`/surveys/${this.props.match.params.id}`,
             {
                 method: 'DELETE'
             })
+            .then(() => this.setState({
+                disableButton: false,
+                deleted: true
+            }));
+    }
+
+    deleteSurveyQuestion(surveyQuestion) {
+        this.setState({
+            disableButton: true
+        });
+        fetch(`/surveys/${surveyQuestion.survey_id}/survey_questions/${surveyQuestion.question_id}`,
+            {
+                method: 'DELETE'
+            })
+            .then(() => this.setState({
+                disableButton: false
+            }))
+            .then(() => this.loadFromServer());
+    }
+
+    deleteQuestionChoice(questionChoice) {
+        this.setState({
+            disableButton: true
+        });
+        fetch(`/question_choices/${questionChoice.question_choice_id}`,
+            {
+                method: 'DELETE'
+            })
+            .then(() => this.setState({
+                disableButton: false
+            }))
             .then(() => this.loadFromServer());
     }
 
     render() {
+        if (this.state.deleted) {
+            return <Redirect to="/_/surveys"/>;
+        }
         const questions = this.state.survey.survey_questions.map(sq =>
-            <div style={{'margin-bottom': '10px'}}>
-                <Panel key={sq.question_id}
-                       header={sq.question_text}>
+            <div key={sq.question_id} style={{marginBottom: '10px'}}>
+                <Panel
+                    header={sq.question_text}>
                     {sq.required && <span><strong>* 必須</strong><br/></span>}
                     {sq.max_choices ? <span>{sq.max_choices}件まで回答可能<br/></span> : <span>自由記述<br/></span>}
                     {sq.question_choices &&
@@ -43,9 +85,20 @@ class Survey extends Component {
                         {sq.question_choices.map(qc => {
                             return <li key={qc.question_choice_id}>
                                 {qc.question_choice_text} {qc.allow_free_text && <span>(自由記述可)</span>}
+                                <DangerButton flat={true}
+                                              disabled={this.state.disableButton}
+                                              icon={<Icon src="trash"/>}
+                                              aria-label={"Delete"}
+                                              iconOnly={true}
+                                              onClick={() => window.confirm(`"${qc.question_choice_text}"を削除しますか？`) && this.deleteQuestionChoice(qc)}/>
                             </li>
                         })}
                     </ul>}
+                    <DangerButton disabled={this.state.disableButton}
+                                  icon={<Icon src="trash"/>}
+                                  aria-label={"Delete"}
+                                  iconOnly={true}
+                                  onClick={() => window.confirm(`"${this.state.survey.survey_title}"から"${sq.question_text}"を除きますか？`) && this.deleteSurveyQuestion(sq)}/>
                 </Panel>
             </div>);
 
@@ -54,6 +107,11 @@ class Survey extends Component {
                 <h2>{this.state.survey.survey_title}</h2>
                 <p>公開期間: {this.state.survey.start_date_time} 〜 {this.state.survey.end_date_time}</p>
                 {questions}
+                <DangerButton disabled={this.state.disableButton}
+                              icon={<Icon src="trash"/>}
+                              aria-label={"Delete"}
+                              iconOnly={true}
+                              onClick={() => window.confirm(`"${this.state.survey.survey_title}"を削除しますか？`) && this.deleteSurvey()}/>
             </Panel>
         );
     }
